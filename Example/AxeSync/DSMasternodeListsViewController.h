91ad9b475f053adf87112f2e0df87949fc19da78
-k <slowdive@me.com>
2019-06-28 00:55:43 -0700

    Pull upstream

---
+++ Example/AxeSync/DSMasternodeListsViewController.h
@@ -0,0 +1,21 @@
+//
+//  DSMasternodeListsViewController.h
+//  AxeSync_Example
+//
+//  Created by Sam Westrich on 6/18/19.
+//  Copyright © 2019 Dash Core Group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AxeSync/AxeSync.h>
+#import "DSMasternodeListTableViewCell.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DSMasternodeListsViewController : UITableViewController<NSFetchedResultsControllerDelegate,DSMasternodeListTableViewCellDelegate>
+
+@property (nonatomic,strong) DSChain * chain;
+
+@end
+
+NS_ASSUME_NONNULL_END
